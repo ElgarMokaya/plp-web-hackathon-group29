633a5cd4791485d097d4b8e6664352fe8a7b1c9a
ElgarMokaya <elgarmokaya01@gmail.com>
2022-08-07 05:20:39 +0300

    fixed bugs

---
+++ portfolio-website/index.js
@@ -0,0 +1,14 @@
+//
+//function toggleMobileMenu(this){
+   // menubar.classList.toggle('open');
+//}
+//
+
+function toggle(){
+var y = document.getElementById("mylinks");
+if(x.style.display != "block") {
+    x.style.display = "none";
+}else{
+    x.style.display = "block"
+}
+}
